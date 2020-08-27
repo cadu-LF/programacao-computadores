ebb62863652202112cb8b21b54780d13d8545f55
Carlos Eduardo Lourenço <nice.ice312@gmail.com>
2020-08-26 21:44:05 -0300

    Modularizei a entrada e o calculo da média, deixando o programa mais organizado

--- lista-exe7.js
+++ lista-exe7.js
@@ -1,30 +1,29 @@
 function principal(){
 
     var nros = []
-    do{
-
-        nros.push(prompt(`Infome um numero: `))
-        var resp = prompt(`deseja sair? s/n`)
-
-    }
-    while(resp != 's')
-
-    for(var i = 0; i < nros.length; i++){
-        nros[i] = Number(nros[i])
-    }
-
-    resp = media(nros)
-    console.log(resp)
+    entrada(nros)
+    console.log(nros)
+    media(nros)
 }
 
-function media(nros){
+function entrada(vetor){
 
-    var media = 0
-    for(var i = 0; i < nros.length; i++){
-        media += nros[i]
+    var numero = Number(prompt(`Informe um número: `))
+    do{
+
+        vetor.push(numero)
+        numero = Number(prompt(`Informe um número, positivo continua, negativo sai: `))
+        
     }
+    while(numero >= 0)
+}
 
-    return media / i
+function media(vet){
+    var soma = 0
+    for(var i=0; i<vet.length; i++){
+        soma += ve[i]
+    }
+    console.log(`A média é ${soma}`)
 }
 
 principal()
